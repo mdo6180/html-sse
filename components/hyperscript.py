65f2225cdf73d27963919416f2701f34dd54f72b
Minh Quan Do <mdo6180@gmail.com>
2024-05-20 02:05:25 -0400

    removed htmx and htmx-sse extension in <head> of hyperscript example

--- components/hyperscript.py
+++ components/hyperscript.py
@@ -6,12 +6,6 @@ def homepage():
             <meta charset="UTF-8">
             <title>Anacostia Console</title>
 
-            <!-- non-minified Htmx -->
-            <script src="/static/js/htmx.js" type="text/javascript"></script>
-
-            <!-- htmx SSE extension -->
-            <script src="/static/js/sse.js"></script>
-
             <!-- minified _Hyperscript (Whole 9 Yards version) -->
             <script src="/static/js/_hyperscript_w9y.min.js"></script>
 
